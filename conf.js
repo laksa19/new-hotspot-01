4fe00b4cbf2525d6d6a8cba49a739c7333448c98
Laksamadi Guko <elru.lakusa@gmail.com>
2020-06-04 22:56:55 +0700

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,14 @@
+var config = {
+
+loginvc : "Masukkan Kode Voucher kemudian klik Connect.",
+loginup : "Masukkan Username dan Password <br> kemudian klik Connect.",
+voucherCode : "Kode Voucher",
+setCase : "none", // lowercase, uppercase or none
+defaultMode : "voucher", // voucher or member
+theme : "default", // default, dark, lite
+
+
+url : "https://demo.mikhmon.online", 
+SessionName : "demo", 
+
+}
